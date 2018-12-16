f0c0cc22eb5aca2787c3bcc69795d621c4dee087
soareseric <contato.eric.sm@gmail.com>
2018-12-16 21:38:01 -0200

    Projeto criado, inicio request/response xml

---
+++ src/main/java/com/ericsoares/soap/webservices/soapmanagement/SoapManagementApplication.java
@@ -0,0 +1,14 @@
+package com.ericsoares.soap.webservices.soapmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapManagementApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/ericsoares/soap/webservices/soapmanagement/SoapManagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ericsoares.soap.webservices.soapmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoapManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
