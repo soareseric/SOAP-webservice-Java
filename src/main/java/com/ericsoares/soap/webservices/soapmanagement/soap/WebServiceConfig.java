b3c8fc73b24b1a72d7e172de60390a1fb2ce961a
soareseric <contato.eric.sm@gmail.com>
2018-12-17 14:03:50 -0200

    Configured Message Dispatcher Servlet

---
+++ src/main/java/com/ericsoares/soap/webservices/soapmanagement/soap/WebServiceConfig.java
@@ -0,0 +1,28 @@
+package com.ericsoares.soap.webservices.soapmanagement.soap;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+//Enable Spring Web Services
+//Spring Configuration
+@EnableWs
+@Configuration
+public class WebServiceConfig {
+	//MessageDispatcherServlet
+		//ApplicationContext
+	//url -> /ws/*
+	
+	@Bean
+	ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+		MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+		messageDispatcherServlet.setApplicationContext(context);
+		messageDispatcherServlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
+	}
+	
+	
+}
