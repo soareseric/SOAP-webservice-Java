eea8f0bbdb6225d4f88c8959720d928bb26a66ee
soareseric <contato.eric.sm@gmail.com>
2018-12-18 10:09:21 -0200

    Include basic security password with WS Security

--- src/main/java/com/ericsoares/soap/webservices/soapmanagement/soap/WebServiceConfig.java
+++ src/main/java/com/ericsoares/soap/webservices/soapmanagement/soap/WebServiceConfig.java
@@ -1,11 +1,18 @@
 package com.ericsoares.soap.webservices.soapmanagement.soap;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.wss4j2.callback.SimplePasswordValidationCallbackHandler;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -15,11 +22,11 @@ import org.springframework.xml.xsd.XsdSchema;
 //Spring Configuration
 @EnableWs
 @Configuration
-public class WebServiceConfig {
-	//MessageDispatcherServlet
-		//ApplicationContext
-	//url -> /ws/*
-	
+public class WebServiceConfig extends WsConfigurerAdapter {
+	// MessageDispatcherServlet
+	// ApplicationContext
+	// url -> /ws/*
+
 	@Bean
 	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
 		MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
@@ -27,28 +34,46 @@ public class WebServiceConfig {
 		messageDispatcherServlet.setTransformWsdlLocations(true);
 		return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
 	}
-	
-	//  /ws/Courses/ws
-		//PortType - CoursePort
-		//namespace - http://ericsoares.com/courses
-	//  courses-details.xsd
-	
-	@Bean(name="courses")
+
+	// /ws/Courses/ws
+	// PortType - CoursePort
+	// namespace - http://ericsoares.com/courses
+	// courses-details.xsd
+
+	@Bean(name = "courses")
 	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema coursesSchema) {
 		DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
 		definition.setPortTypeName("CoursePort");
 		definition.setTargetNamespace("http://ericsoares.com/courses");
 		definition.setLocationUri("/ws");
 		definition.setSchema(coursesSchema);
-		
+
 		return definition;
 	}
-	
-	
+
 	@Bean
 	public XsdSchema coursesSchema() {
 		return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
 	}
-	
-	
+
+	@Bean
+	public XwsSecurityInterceptor securityInterceptor() {
+		XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+		securityInterceptor.setCallbackHandler(callbackHandler());
+		securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
+		return securityInterceptor;
+	}
+
+	@Bean
+	public SimplePasswordValidationCallbackHandler callbackHandler() {
+		SimplePasswordValidationCallbackHandler handler = new SimplePasswordValidationCallbackHandler();
+		handler.setUsersMap(Collections.singletonMap("user", "password"));
+		return handler;
+	}
+
+	@Override
+	public void addInterceptors(List<EndpointInterceptor> interceptors) {
+		interceptors.add(securityInterceptor());
+	}
+
 }
